3a9ee5d6077f4b8323036ac22e3c2e688ec47ce7
gary <igyueming@gmail.com>
2016-07-19 15:15:37 +0800

    touch for mouth

--- source/src/js/index.js
+++ source/src/js/index.js
@@ -20,12 +20,20 @@ if (navigator.platform.indexOf('Win32') !== -1) {
 }*/
 document.addEventListener('touchstart', touch, false);
 document.addEventListener('touchmove', touch, false);
-document.addEventListener('touchend', touch, false);
+document.addEventListener('touchend', function(e) {
+	var eye1 = document.getElementsByClassName('eyes')[0],
+		eye2 = document.getElementsByClassName('eyes')[1],
+		mouth = document.getElementsByClassName('mouth')[0];
+	eye1.style.transform = 'translate3d(0, 0, 0)';
+	eye2.style.transform = 'translate3d(0, 0, 0)';
+	mouth.style.transform = 'rotateZ(0)';
+}, false);
 
 function touch(event) {
 	var event = event || window.event,
 		eye1 = document.getElementsByClassName('eyes')[0],
 		eye2 = document.getElementsByClassName('eyes')[1],
+		mouth = document.getElementsByClassName('mouth')[0],
 		//页面中心坐标
 		oX = Math.round(document.documentElement.clientWidth/2),
 		oY = Math.round(document.documentElement.clientHeight/2),
@@ -37,23 +45,23 @@ function touch(event) {
 		stepY = Math.round(oY/15),
 		//偏移量
 		x = 0,
-		y = 0;
+		y = 0,
+		deg = 0;
 	//计算偏移量
-	x = Math.round((oX - tX)/stepX);
-	y = Math.round((oY - tY)/stepY);
+	x = Math.round((tX - oX)/stepX);
+	y = Math.round((tY - oY)/stepY);
+	deg = Math.round((tX - oX)/stepX);
 
 	switch(event.type) {
 		case 'touchstart':
 			eye1.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
 			eye2.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
+			mouth.style.transform = 'rotateZ(' + deg + 'deg)';
 			break;
 		case 'touchmove':
 			eye1.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
 			eye2.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
-			break;
-		case 'touchend':
-			eye1.style.transform = 'translate3d(0, 0, 0)';
-			eye2.style.transform = 'translate3d(0, 0, 0)';
+			mouth.style.transform = 'rotateZ(' + deg + 'deg)';
 			break;
 	}
 }
