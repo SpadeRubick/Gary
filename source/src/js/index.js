5c63b2eb5add2047be03a33c4d3161b02d1ad963
gary <igyueming@gmail.com>
2016-07-19 14:44:21 +0800

    remove animation, add touch event

--- source/src/js/index.js
+++ source/src/js/index.js
@@ -18,3 +18,42 @@ path.setAttribute('d', dTxt);
 if (navigator.platform.indexOf('Win32') !== -1) {
     _remove_target.parentNode.removeChild(_remove_target);
 }*/
+document.addEventListener('touchstart', touch, false);
+document.addEventListener('touchmove', touch, false);
+document.addEventListener('touchend', touch, false);
+
+function touch(event) {
+	var event = event || window.event,
+		eye1 = document.getElementsByClassName('eyes')[0],
+		eye2 = document.getElementsByClassName('eyes')[1],
+		//中心坐标
+		oX = Math.round(document.documentElement.clientWidth/2),
+		oY = Math.round(document.documentElement.clientHeight/2),
+		//touch坐标
+		tX = Math.round(event.touches[0].clientX),
+		tY = Math.round(event.touches[0].clientY),
+		//步长
+		stepX = Math.round(wx/2/10),
+		stepY = Math.round(wy/2/15),
+		//偏移量
+		x = 0,
+		y = 0;
+	//计算偏移量
+	x = Math.round((oX - tX)/setpX);
+	y = Math.round((oY - tY)/setpY);
+
+	switch(event.type) {
+		case 'touchstart':
+			eye1.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
+			eye2.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
+			break;
+		case 'touchmove':
+			eye1.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
+			eye2.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0)';
+			break;
+		case 'touchend':
+			eye1.style.transform = 'translate3d(0, 0, 0)';
+			eye2.style.transform = 'translate3d(0, 0, 0)';
+			break;
+	}
+}
